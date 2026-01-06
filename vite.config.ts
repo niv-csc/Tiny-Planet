a7396fb0aaf3cc981a2a11f0f87a4bd67c176847
niv-csc <nivriti3266@gmail.com>
2026-01-06 21:15:04 +0400

    Fix Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Tiny-Planet/", // 👈 this is the fix for GitHub Pages
   server: {
     host: "::",
     port: 8080,
